4f0fcba4e764363a27223826492ee7b982c5e619
MotraHassan <133110048+MotraHassan@users.noreply.github.com>
2023-06-02 14:19:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let motra = document.querySelector('.motra');
+window.onscroll = function(){
+    let value = scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top = value *3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value  + 'px';
+    boat.style.top = value  + 'px';
+    boat.style.left = value * 3 + 'px';
+    motra.style.fontSize = value + 'px';
+    if(scrollY >= 67){
+        motra.style.fontSize = 67 + 'px';
+        motra.style.position =  'fixed';
+        if(scrollY >= 360){
+            motra.style.display =  'none';
+        }else{
+            motra.style.display =  'block';
+        }
+
+        if(scrollY >= 127){
+            document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)'
+        }else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+        }
+    }
+}
